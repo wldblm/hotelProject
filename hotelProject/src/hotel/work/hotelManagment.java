ba2e5382d7106bcb4b55366af3d23583791ae6fb
Walid Bilem <wldblm@icloud.com>
2021-10-14 16:28:25 +0200

    comments

--- hotelProject/src/hotel/work/hotelManagment.java
+++ hotelProject/src/hotel/work/hotelManagment.java
@@ -4,35 +4,16 @@ import hotel.rooms.Room;
 
 public class hotelManagment {
 	private Room hotel[];
+	
 	public hotelManagment() {
 		
 		hotel = generate();
-		
-		for (int i = 0; i < hotel.length; i++) {
-			System.out.println(hotel[i].getRoomType());
-			System.out.println(hotel[i].getView());
-			System.out.println(hotel[i].getMaxPer());
-			System.out.println(hotel[i].getSize());
-			System.out.println(hotel[i].getPrice());
-			String options[] = hotel[i].getOptions();
-			for (int j = 0; j < options.length; j++) {
-				System.out.println(options[j]);
-			}
-			System.out.println("");
-		}
-		
-		
-	}
-
-	public Room[] getHotel() {
-		return hotel;
-	}
-
-	public void setHotel(Room hotel[]) {
-		this.hotel = hotel;
+	
 	}
 	
+	// Generation des chambres d'hotel
 	public Room[] generate() {
+		
 		String listeChambresCsv [] ={"Type chambre;Taille;Vues;Occupation;tarif;Nombre de chambre;Options",
 				"Chambre Vue Piscine;44 mètres carrés;Piscine Centrale;2 adultes et 2 enfants de moins de 12 ans;300;7;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
 				"Chambre Vue Jardin;44 mètres carrés;Jardin, Forêt ou Golf;2 adultes et 2 enfants de moins de 12 ans;314;12;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
@@ -42,8 +23,12 @@ public class hotelManagment {
 				"Suite Executive;140 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;550;5;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
 				"Suite Ambassadeur;230 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;1650;7;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
 				"Suite Royale;342  mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;2400;4;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24"};
+		
 		hotel = new Room[65];
+		// nbPrev correspond à la dernière chambre instanciée
 		int nbPrev = 0;
+		
+		//On boucle sur le Csv et à chaque itération on crée un tableau de chaque attributs de classe qu'on met dans des variables puis on instance une room nb fois dans le tableau hotel
 		for (int i = 1; i < listeChambresCsv.length; i++) {
 			
 			String tab[] = listeChambresCsv[i].split(";");
@@ -58,7 +43,6 @@ public class hotelManagment {
 					hotel[nbPrev] = new Room(roomType, view, maxPer, size, price,options);
 					nbPrev++;
 				}
-			
 			
 		}
 		
