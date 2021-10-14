a82a167bf668583793c8cf2886fe6e106d9f1e10
Walid Bilem <wldblm@icloud.com>
2021-10-14 15:53:53 +0200

    generation de l'hotel a partir du faux csv

--- hotelProject/src/hotel/work/hotelManagment.java
+++ hotelProject/src/hotel/work/hotelManagment.java
@@ -4,9 +4,24 @@ import hotel.rooms.Room;
 
 public class hotelManagment {
 	private Room hotel[];
-	
 	public hotelManagment() {
-		// TODO Auto-generated constructor stub
+		
+		hotel = generate();
+		
+		for (int i = 0; i < hotel.length; i++) {
+			System.out.println(hotel[i].getRoomType());
+			System.out.println(hotel[i].getView());
+			System.out.println(hotel[i].getMaxPer());
+			System.out.println(hotel[i].getSize());
+			System.out.println(hotel[i].getPrice());
+			String options[] = hotel[i].getOptions();
+			for (int j = 0; j < options.length; j++) {
+				System.out.println(options[j]);
+			}
+			System.out.println("");
+		}
+		
+		
 	}
 
 	public Room[] getHotel() {
@@ -15,6 +30,39 @@ public class hotelManagment {
 
 	public void setHotel(Room hotel[]) {
 		this.hotel = hotel;
+	}
+	
+	public Room[] generate() {
+		String listeChambresCsv [] ={"Type chambre;Taille;Vues;Occupation;tarif;Nombre de chambre;Options",
+				"Chambre Vue Piscine;44 mètres carrés;Piscine Centrale;2 adultes et 2 enfants de moins de 12 ans;300;7;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
+				"Chambre Vue Jardin;44 mètres carrés;Jardin, Forêt ou Golf;2 adultes et 2 enfants de moins de 12 ans;314;12;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
+				"Chambre Vue Océan;44 mètres carrés;Partielle Océan et Forêt ou Golf;2 adultes et 2 enfants de moins de 12 ans;350;8;Fer et planche à repasser sur demande|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
+				"Chambre vue imprenable sur l'océan;44 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;350;10;Fer et planche à repasser sur demande|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
+				"Suite CDA;82 mètres carrés;Océan et Golf;2 adultes et 2 enfants de moins de 12 ans;450;12;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
+				"Suite Executive;140 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;550;5;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
+				"Suite Ambassadeur;230 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;1650;7;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
+				"Suite Royale;342  mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;2400;4;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24"};
+		hotel = new Room[65];
+		int nbPrev = 0;
+		for (int i = 1; i < listeChambresCsv.length; i++) {
+			
+			String tab[] = listeChambresCsv[i].split(";");
+			String roomType = tab[0];
+			String size = tab[1];
+			String view = tab[2];
+			String maxPer = tab[3];
+			String price = tab[4];
+			int nb = Integer.parseInt(tab[5]);
+			String options[] = tab[6].split("\\|");
+				for (int k = 0; k < nb; k++) {
+					hotel[nbPrev] = new Room(roomType, view, maxPer, size, price,options);
+					nbPrev++;
+				}
+			
+			
+		}
+		
+		return hotel;
 	}
 	
 	
